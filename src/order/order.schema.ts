a00cbc306071c22767f7e25f9fb99b73d739723e
parthsunilmodi <parthmodi.developer@gmail.com>
2023-06-14 20:28:52 +0530

    fix: Order api

--- src/order/order.schema.ts
+++ src/order/order.schema.ts
@@ -4,6 +4,37 @@ import { Book } from '../book/book.schema';
 import { User } from '../users/user.schema';
 
 export type OrderDocument = Order & Document;
+export type BookDocument = Book & Document;
+
+@Schema({ versionKey: false })
+export class Books {
+  @Prop({ type: mongoose.Schema.Types.ObjectId, ref: 'books', required: true })
+  bookId: Book;
+
+  @Prop({ required: true })
+  quantity: number;
+
+  @Prop({ required: true })
+  title: string;
+
+  @Prop({ required: true, unique: true })
+  writer: string;
+
+  @Prop({
+    default:
+      'https://images-na.ssl-images-amazon.com/images/I/51Ga5GuElyL._AC_SX184_.jpg',
+  })
+  coverImage: string;
+
+  @Prop({ default: 5 })
+  points: number;
+
+  @Prop({
+    default: 'fiction',
+    enum: ['fiction', 'non-fiction', 'science', 'essay'],
+  })
+  tag: string;
+}
 
 @Schema({ versionKey: false })
 export class Order {
@@ -11,7 +42,7 @@ export class Order {
   userId: User;
 
   @Prop({ type: mongoose.Schema.Types.Array, required: true })
-  books: Book;
+  books: Books;
 
   @Prop({ default: 0 })
   total: number;
